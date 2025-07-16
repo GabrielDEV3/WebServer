3d84384ebd442e83c2132540f51285eb4b407114
GabrielDEV3 <154340703+GabrielDEV3@users.noreply.github.com>
2025-07-16 03:25:02 -0300

    Create room.js

---
+++ room.js
@@ -0,0 +1,37 @@
+const Player = require("./player.js");
+
+class Room {
+  constructor() {
+    this.players = new Map(); // uuid => Player
+  }
+
+  async getAll() {
+    return Array.from(this.players.values());
+  }
+
+  async get(uuid) {
+    return this.players.get(uuid) || null;
+  }
+
+  async add(uuid, name = "Player") {
+    if (!this.players.has(uuid)) {
+      const player = new Player(uuid, name);
+      this.players.set(uuid, player);
+    }
+    return true;
+  }
+
+  async update(uuid, json) {
+    const player = this.players.get(uuid);
+    if (!player) return false;
+
+    player.setJSON(json);
+    return true;
+  }
+
+  async remove(uuid) {
+    return this.players.delete(uuid);
+  }
+}
+
+module.exports = new Room();
